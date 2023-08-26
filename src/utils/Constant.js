ac68c828b7dba2be57447861505ca5aeb92e059b
Usama <usama.amjad32@gmail.com>
2023-08-27 04:21:12 +0500

    added

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,4 +1,5 @@
 export const VIDEOBASEURL = 'https://video.bunnycdn.com/';
 export const AUDIOBASEURL = 'https://api.bunny.net/';
-export const SENTERTAINMENT_DB = 'http://localhost:3002';
-export const socket = new WebSocket('ws://localhost:3002'); // Replace with your server URL
+export const SENTERTAINMENT_DB = 'https://sentertainment-backend.onrender.com'; // 'http://localhost:3002';
+export const socket = new WebSocket('ws://sentertainment-backend.onrender.com'); // Replace with your server URL
+// export const socket = new WebSocket('ws://localhost:3002'); // Replace with your server URL
